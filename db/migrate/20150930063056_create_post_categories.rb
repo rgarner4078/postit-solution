a7c51a670eeee167e7c1a101a62036306de0b654
Ryan Garner <rgarner@Ryans-MacBook-Pro.local>
2015-10-26 13:55:20 -0400

    once again editing migration

--- db/migrate/20150930063056_create_post_categories.rb
+++ db/migrate/20150930063056_create_post_categories.rb
@@ -1,6 +1,6 @@
 class CreatePostCategories < ActiveRecord::Migration
   def change
-    create_table :post_catagories do |t|
+    create_table :post_categories do |t|
       t.integer :category_id, :post_id
       t.timestamps
     end
