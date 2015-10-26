df53658170d46e11adc652722bf6a4087a3ae825
Ryan Garner <rgarner@Ryans-MacBook-Pro.local>
2015-10-26 13:09:12 -0400

    lesson 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,57 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :vote]
+  before_action :require_user, except: [:show, :index]
+
   def index
-    @posts = Post.all
+    @posts = Post.all.sort_by{|x| x.total_votes}.reverse
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def show 
+    @comment = Comment.new
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.creator = current_user
 
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated."
+      redirect_to posts_path(@post)
+    else
+    end
   end
+
+  def vote
+    vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
+    if vote.valid?
+      flash[:notice] = "Your vote was counted."
+    else
+      flash[:notice] = "You can only vote on a vote once."
+    end
+    redirect_to :back
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description, category_ids:[])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
